ad2df6eae3da8fb0c0b0a9b1d9f680c3a3853c68
Michael Tedeschi <mike.tedeschi@gmail.com>
2016-03-03 18:06:39 -0500

    Add events, pages, artwork; include lightgallery files

---
+++ artwork-detail-page.php
@@ -0,0 +1,29 @@
+<?php include('inc/header.html'); ?>
+
+<main>
+    <section class="container-fluid">
+        <div class="artwork-hero-inner">                
+            <div class="artwork-hero-image border">
+                <img src="http://placehold.it/720x600" alt="#">
+                <img src="http://placehold.it/720x600" alt="#">
+                <img src="http://placehold.it/720x600" alt="#">
+                <img src="http://placehold.it/720x600" alt="#">
+                <img src="http://placehold.it/720x600" alt="#">
+            </div>
+            <div class="artwork-hero-details-container">
+                <div class="artwork-hero-details">
+                    <h3>Artist Installation</h3>
+                    <h1>Salisbury</h1>
+                    <p>A short experimental film about the choice of migration and the choice of remaining. It is shot in Salisbury, North Carolina by Kevin Jerome Everson.</p>
+                    <a href="#" class="btn btn-default btn-lg btn-inverted"><span class="glyphicon glyphicon-picture"></span> View the Gallery</a><br/>
+                    <a href="#" class="btn btn-default btn-lg btn-inverted"><span class="glyphicon glyphicon-facetime-video"></span> Watch the Artist Statement</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="artwork-content-section">
+        
+    </section>
+</main>
+
+<?php include('inc/footer.html'); ?>
---
+++ events.php
@@ -0,0 +1,9 @@
+<?php include('inc/header.html'); ?>
+
+<main>
+    <section class="page container">
+        
+    </section>
+</main>
+
+<?php include('inc/footer.html'); ?>
