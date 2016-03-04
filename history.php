127d9cbf75bcfef75f6f385840ee2635dea4e387
Michael Tedeschi <mike.tedeschi@gmail.com>
2016-03-04 14:13:13 -0500

    Add history landing page

---
+++ history.php
@@ -0,0 +1,73 @@
+<?php include('inc/header.html'); ?>
+
+<main>
+    <section class="page container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12">
+                <h2>History & Context</h2>
+                <p>Nullam sed tempor neque, non feugiat orci. Morbi placerat mollis elit, sit amet consectetur lorem condimentum lobortis. Etiam pulvinar nibh justo, nec sagittis lacus ultricies ut. Donec volutpat lacinia arcu eget lacinia.</p>
+            </div>
+    </section>
+    <section class="artwork-array">
+        <div class="container">
+            <div class="flex-container">
+                <a class="artwork-container history-context">
+                    <div class="artwork-content">
+                        <h4>History & Context</h4>
+                        <h3>The Great Migration's Impact on Philadelphia</h3>
+                    </div>
+                    <div class="overlay border"></div>
+                    <img src="http://placehold.it/400x350" alt="#">
+                </a>
+                <a class="artwork-container history-context">
+                    <div class="artwork-content">
+                        <h4>History & Context</h4>
+                        <h3>Refugees throughout History - Connecting the Great Migration to Modern Day</h3>
+                    </div>
+                    <div class="overlay border"></div>
+                    <img src="http://placehold.it/400x550" alt="#">
+                </a>
+                <a class="artwork-container history-context">
+                    <div class="artwork-content">
+                        <h4>History & Context</h4>
+                        <h3>The Great Migration's Impact on Philadelphia</h3>
+                    </div>
+                    <div class="overlay border"></div>
+                    <img src="http://placehold.it/400x350" alt="#">
+                </a>
+                <a class="artwork-container history-context">
+                    <div class="artwork-content">
+                        <h4>History & Context</h4>
+                        <h3>The Great Migration's Impact on Philadelphia</h3>
+                    </div>
+                    <div class="overlay border"></div>
+                    <img src="http://placehold.it/400x350" alt="#">
+                </a>
+                <a class="artwork-container history-context">
+                    <div class="artwork-content">
+                        <h4>History & Context</h4>
+                        <h3>The Great Migration's Impact on Philadelphia</h3>
+                    </div>
+                    <div class="overlay border"></div>
+                    <img src="http://placehold.it/400x350" alt="#">
+                </a>
+                <a class="artwork-container history-context">
+                    <div class="artwork-content">
+                        <h4>History & Context</h4>
+                        <h3>The Great Migration's Impact on Philadelphia</h3>
+                    </div>
+                    <div class="overlay border"></div>
+                    <img src="http://placehold.it/400x350" alt="#">
+                </a>
+            </div>
+            <nav>
+                <ul class="pager">
+                    <li class="disabled"><a href="#">Previous</a></li>
+                    <li><a href="#">Next</a></li>
+                </ul>
+            </nav>
+        </div>
+    </section>
+</main>
+
+<?php include('inc/footer.html'); ?>
