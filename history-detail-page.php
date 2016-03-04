933317ae5bf54804ded42167abd3bbd87e452cdc
Michael Tedeschi <mike.tedeschi@gmail.com>
2016-03-04 15:17:53 -0500

    Add history detail page, rework hero styling

---
+++ history-detail-page.php
@@ -0,0 +1,30 @@
+<?php include('inc/header.html'); ?>
+
+<main>
+    <section class="container-fluid hero">
+        <img src="http://placehold.it/1680x750" alt="#">
+        <div class="hero-details border">
+            <h3>History & Context</h3>
+            <h1>The Great Migration's Impact on Philadelphia</h1>
+        </div>
+        <div class="overlay"></div>
+    </section>
+    <section class="page container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc venenatis augue quis pulvinar congue. Cras accumsan ligula a ante porta commodo. Nullam pretium quam nec leo ullamcorper feugiat. Aliquam vitae pellentesque est. Suspendisse tincidunt at ipsum in tempor. Quisque quis leo at nisi fermentum scelerisque eget in massa. Aliquam efficitur odio et feugiat maximus. Duis blandit magna quis sem sodales, sit amet sodales odio faucibus. Vivamus vehicula eros dignissim, vehicula magna interdum, molestie ante. Donec ac libero lacinia, suscipit arcu sit amet, luctus urna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed eget turpis ac dolor elementum eleifend. Donec libero orci, scelerisque non leo ac, euismod eleifend augue.</p>
+                <img src="http://placehold.it/900x300" alt="#">
+                <p>Maecenas tincidunt urna vitae sodales blandit. Sed ac ante mauris. Vestibulum non nibh ac eros vestibulum suscipit. Praesent neque massa, aliquam non hendrerit sit amet, iaculis nec odio. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi nec convallis erat. Quisque lectus enim, aliquet vel erat a, maximus efficitur neque. Nam id orci lacinia, imperdiet urna et, placerat augue. In condimentum ipsum vitae dui feugiat, quis mollis neque elementum. Sed ultricies augue nunc, id fermentum ante scelerisque id. Ut diam dolor, lobortis quis nunc sed, pretium tempor risus.</p>
+                <p>Nullam sed tempor neque, non feugiat orci. Morbi placerat mollis elit, sit amet consectetur lorem condimentum lobortis. Etiam pulvinar nibh justo, nec sagittis lacus ultricies ut. Donec volutpat lacinia arcu eget lacinia. Donec tincidunt ac turpis sed viverra. In dictum augue sit amet mauris semper, et molestie lorem pretium. Fusce justo ex, congue id consectetur id, cursus ut nunc. In sit amet tellus justo. Etiam pulvinar non nunc nec sollicitudin.</p>
+                <figure role="group" class="caption caption-img align-right">
+                    <img src="http://placehold.it/900x400" alt="#">
+                    <figcaption>This is a test caption.</figcaption>
+                </figure>
+                <p>Morbi non dolor at elit maximus facilisis non ut elit. Ut et odio gravida, euismod nisl et, consectetur felis. Nullam fermentum urna nec sagittis iaculis. Vivamus at sapien semper, lobortis nibh ut, malesuada mi. Vestibulum fermentum, erat in scelerisque tincidunt, mi eros consectetur turpis, ac aliquet nisl ex id nunc. Nunc quis fermentum enim, eu gravida massa. Ut a lacus ut dolor dignissim aliquet eu sodales nulla. Aliquam vehicula quis nisl et mollis.</p>
+                <p>Maecenas ultricies tristique mollis. Suspendisse sagittis ex vitae leo mattis ultricies. Suspendisse potenti. Duis congue magna vitae risus varius pretium. Maecenas eget tristique sem, a bibendum nulla. Nunc pellentesque ex lacus, eu blandit elit lobortis sit amet. In sed consectetur dolor, sed eleifend neque. Praesent eget facilisis metus. Donec justo quam, aliquet vitae dolor a, tristique scelerisque dolor. Maecenas eu tempor dui. Nunc tincidunt purus in viverra varius. In mauris magna, euismod eget sem id, volutpat consectetur dui. Nulla lobortis neque dignissim, elementum massa sit amet, pellentesque ipsum. Phasellus iaculis in nulla vel efficitur. Phasellus vel massa eros. Phasellus et ipsum molestie, condimentum sapien sed, suscipit urna.</p>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php include('inc/footer.html'); ?>
