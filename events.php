f2c71f14e42533f8f9b2fba787a92ef9c7e447f8
Michael Tedeschi <mike.tedeschi@gmail.com>
2016-03-04 12:50:18 -0500

    Style events page, refine header footer areas

--- events.php
+++ events.php
@@ -2,7 +2,52 @@
 
 <main>
     <section class="page container">
-        
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12">
+                <h2>Upcoming Events & Programming</h2>
+                <p>Nullam sed tempor neque, non feugiat orci. Morbi placerat mollis elit, sit amet consectetur lorem condimentum lobortis. Etiam pulvinar nibh justo, nec sagittis lacus ultricies ut. Donec volutpat lacinia arcu eget lacinia.</p>
+                <div class="events">
+                    <div class="event row">
+                        <div class="col-xs-4 col-sm-3">
+                            <div class="date-stamp">
+                                <div class="date-stamp-day">26</div>
+                                <div class="date-stamp-month">May</div>
+                            </div>
+                        </div>
+                        <div class="col-xs-8 col-sm-9">
+                            <h5>West Chester University</h5>
+                            <h1>Excerpts from Goin' North</h1>
+                            <a href="#">Attend this event</a><span class="divider hidden-xs"></span><a href="#">Get directions</a>
+                        </div>
+                    </div>
+                    <div class="event row">
+                        <div class="col-xs-4 col-sm-3">
+                            <div class="date-stamp">
+                                <div class="date-stamp-day">26</div>
+                                <div class="date-stamp-month">May</div>
+                            </div>
+                        </div>
+                        <div class="col-xs-8 col-sm-9">
+                            <h5>West Chester University</h5>
+                            <h1>Excerpts from Goin' North</h1>
+                            <a href="#">Attend this event</a><span class="divider hidden-xs"></span><a href="#">Get directions</a>
+                        </div>
+                    </div>
+                    <div class="event row">
+                        <div class="col-xs-4 col-sm-3">
+                            <div class="date-stamp">
+                                <div class="date-stamp-day">26</div>
+                                <div class="date-stamp-month">May</div>
+                            </div>
+                        </div>
+                        <div class="col-xs-8 col-sm-9">
+                            <h5>West Chester University</h5>
+                            <h1>Excerpts from Goin' North</h1>
+                            <a href="#">Attend this event</a><span class="divider hidden-xs"></span><a href="#">Get directions</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </section>
 </main>
 
