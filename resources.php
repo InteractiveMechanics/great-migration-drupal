12e2a632568c0a2e2799bb8968caf92e7471346d
Michael Tedeschi <mike.tedeschi@gmail.com>
2016-03-04 13:41:54 -0500

    Add resources page, style it up

---
+++ resources.php
@@ -0,0 +1,39 @@
+<?php include('inc/header.html'); ?>
+
+<main>
+    <section class="page container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12">
+                <h2>Partner Projects & Other Resources</h2>
+                <p>Nullam sed tempor neque, non feugiat orci. Morbi placerat mollis elit, sit amet consectetur lorem condimentum lobortis. Etiam pulvinar nibh justo, nec sagittis lacus ultricies ut. Donec volutpat lacinia arcu eget lacinia.</p>
+                <div class="resources">
+                    <a href="#" class="resource">
+                        <img src="http://placehold.it/900x400" alt="#">
+                        <div class="overlay"></div>
+                        <div class="resource-details border">
+                            <h3>West Chester University</h3>
+                            <h1>Goin' North</h1>
+                        </div>
+                    </a>
+                    <a href="#" class="resource">
+                        <img src="http://placehold.it/900x400" alt="#">
+                        <div class="overlay"></div>
+                        <div class="resource-details border">
+                            <h3>West Chester University</h3>
+                            <h1>Goin' North</h1>
+                        </div>
+                    </a>
+                    <a href="#" class="resource">
+                        <img src="http://placehold.it/900x400" alt="#">
+                        <div class="overlay"></div>
+                        <div class="resource-details border">
+                            <h3>West Chester University</h3>
+                            <h1>Goin' North</h1>
+                        </div>
+                    </a>
+                </div>
+            </div>
+    </section>
+</main>
+
+<?php include('inc/footer.html'); ?>
