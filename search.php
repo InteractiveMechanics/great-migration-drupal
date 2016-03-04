59de40c9a58158bbb2c4f8100b291ab5f8f4251e
Michael Tedeschi <mike.tedeschi@gmail.com>
2016-03-04 15:45:17 -0500

    Add placeholder for search page, waiting till Drupal integration to style

---
+++ search.php
@@ -0,0 +1,20 @@
+<?php include('inc/header.html'); ?>
+
+<main>
+    <section class="page container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12">
+                <h2>Search the site</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere massa at augue porttitor pharetra. Nullam sit amet convallis turpis. Sed aliquet maximus efficitur. In a elementum velit. Vestibulum consectetur tortor nec nisl luctus faucibus quis non risus.</p>
+                <form>
+                    <input type="search" class="form-control input-lg" placeholder="Search by keyword or title" />
+                </form>
+                <div class="search-results">
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php include('inc/footer.html'); ?>
